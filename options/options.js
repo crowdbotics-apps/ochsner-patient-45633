db3213106b6fa19373c884ec4ad6a15b0964740b
Crowdbotics <team@crowdbotics.com>
2024-01-26 22:51:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ochsner-patient-45633",
+  url: "https://ochsner-patient-45633.botics.co",
+  api: "https://ochsner-patient-45633.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
